1574083a87eea9b73359cafa10f346214ef6b3b3
yukito1226 <ikuradon.kanra@gmail.com>
2024-04-23 21:52:24 +0900

    ログイン機能の枠組み

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import { Login } from "./components/login";
+
+export default function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  )
+}
---
+++ frontend/src/components/login.tsx
@@ -0,0 +1,11 @@
+export const Login = () => {
+    return (
+        <div>
+            <p>ユーザー名：<input></input></p>
+            <p>パスワード：<input></input></p>
+            <div>
+                <button>ログイン</button>
+            </div>
+        </div>
+    )
+}
